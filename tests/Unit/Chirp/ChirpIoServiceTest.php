d277e0a3645fbc85ba64e256782207a180887815
Jessica Mauerhan <jessica.mauerhan@realpage.com>
2017-04-15 20:51:06 -0500

    add stubs

--- tests/Unit/Chirp/ChirpIoServiceTest.php
+++ tests/Unit/Chirp/ChirpIoServiceTest.php
@@ -2,10 +2,24 @@
 
 namespace Test\Unit\Chirp;
 
+use Chirper\Chirp\JsonChirpTransformer;
+use Chirper\Chirp\PersistenceDriver;
 use Test\Unit\TestCase;
 
 class ChirpIoServiceTest extends TestCase
 {
+    /** @var \PHPUnit_Framework_MockObject_MockObject|JsonChirpTransformer */
+    private $transformer;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject|PersistenceDriver */
+    private $persistenceDriver;
+
+    public function __construct()
+    {
+        $this->transformer = $this->createMock(JsonChirpTransformer::class);
+        $this->persistenceDriver = $this->createMock(PersistenceDriver::class);
+    }
+
     public function testCreateSendsJsonFromRequestToTransformer()
     {
     }
