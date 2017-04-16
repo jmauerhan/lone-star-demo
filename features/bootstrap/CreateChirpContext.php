c7a752e7609a585d60922b5f2b2cf7ba76b6c0b2
Jessica Mauerhan <jessica.mauerhan@realpage.com>
2017-04-15 19:54:49 -0500

    When I post the Chirp

--- features/bootstrap/CreateChirpContext.php
+++ features/bootstrap/CreateChirpContext.php
@@ -5,11 +5,15 @@ namespace Test\Behavior\Context;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Faker;
+use GuzzleHttp\Client;
 
 class CreateChirpContext implements Context
 {
     /** @var Faker\Generator */
     private $faker;
+
+    /** @var Client */
+    private $httpClient;
 
     /** @var string */
     private $chirpText;
@@ -17,6 +21,11 @@ class CreateChirpContext implements Context
     public function __construct()
     {
         $this->faker = Faker\Factory::create();
+        $clientOpt = [
+            'base_uri' => 'http://localhost:3000',
+        ];
+        $client = new Client($clientOpt);
+        $this->httpClient = $client;
     }
 
     /**
@@ -25,6 +34,7 @@ class CreateChirpContext implements Context
     public function iWriteAChirpWithOrLessCharacters($numCharacters)
     {
         $this->chirpText = $this->faker->realText($numCharacters);
+
     }
 
     /**
@@ -32,7 +42,17 @@ class CreateChirpContext implements Context
      */
     public function iPostTheChirp()
     {
-        throw new PendingException();
+        $obj = (object)[
+            'data' => (object)[
+                'type' => 'chirp',
+                'id' => 'uuid',
+                'attributes' => (object)[
+                    'text' => $this->chirpText
+                ]
+            ]
+        ];
+
+        $this->httpClient->post('chirp', ['json' => $obj]);
     }
 
     /**
