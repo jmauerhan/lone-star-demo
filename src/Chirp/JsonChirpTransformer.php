f92b94d371b964aaa57bf5f9813f36f91d6f25f2
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-16 16:35:57 -0500

    set up exception

--- src/Chirp/JsonChirpTransformer.php
+++ src/Chirp/JsonChirpTransformer.php
@@ -2,11 +2,15 @@
 
 namespace Chirper\Chirp;
 
+use Chirper\JsonApi\InvalidJsonException;
+
 interface JsonChirpTransformer
 {
     /**
      * @param string $json
      * @return Chirp
+     *
+     * @throws InvalidJsonException
      */
     public function toChirp(string $json): Chirp;
 }
