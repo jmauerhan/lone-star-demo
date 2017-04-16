1946348b26b3860aee0c46084660f3ea3a7b968a
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-16 17:10:25 -0500

    Interface

--- src/Chirp/PersistenceDriver.php
+++ src/Chirp/PersistenceDriver.php
@@ -2,7 +2,15 @@
 
 namespace Chirper\Chirp;
 
+use Chirper\Persistence\PersistenceDriverException;
+
 interface PersistenceDriver
 {
-
+    /**
+     * @param Chirp $chirp
+     * @return bool
+     *
+     * @throws PersistenceDriverException
+     */
+    public function create(Chirp $chirp): bool;
 }
---
+++ src/Persistence/PersistenceDriverException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Chirper\Persistence;
+
+class PersistenceDriverException extends \Exception
+{
+
+}
