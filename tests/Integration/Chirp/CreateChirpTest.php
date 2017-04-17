10e4b7a5e6dd46259ca70faa703726d886447831
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-16 22:38:04 -0500

    integration test

---
+++ tests/Integration/Chirp/CreateChirpTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Test\Integration;
+
+use Chirper\Http\Request;
+use GuzzleHttp\Client;
+use Ramsey\Uuid\Uuid;
+use Test\Unit\TestCase;
+
+class CreateChirpTest extends TestCase
+{
+    private $host = 'http://localhost:3000';
+
+    public function testValidPostReturnsSuccessfulResponse()
+    {
+        $attributes = (object)['text' => 'This is a new Chirp'];
+        $data       = (object)[
+            'type' => 'chirp',
+            'id' => Uuid::uuid4(),
+            'attributes' => $attributes
+        ];
+        $object     = (object)['data' => $data];
+        $payload    = json_encode($object);
+
+        $guzzle   = new Client(['base_uri' => $this->host]);
+        $opt      = ['body' => $payload];
+        $response = $guzzle->post('chirp', $opt);
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals($payload, $response->getBody()->getContents());
+
+    }
+}
