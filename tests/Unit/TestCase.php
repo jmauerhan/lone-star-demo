83af6408fd2c1c459edb979a6f6ccfa345cd2984
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-16 20:06:14 -0500

    fixing constructor

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -9,9 +9,9 @@ class TestCase extends \PHPUnit\Framework\TestCase
     /** @var Faker\Generator */
     protected $faker;
 
-    public function __construct()
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
         $this->faker = Faker\Factory::create();
-        parent::__construct();
+        parent::__construct($name, $data, $dataName);
     }
 }
